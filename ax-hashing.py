2601d3cfd81becda88e4bd87e25e017c4514de7b
Ali Mohammad Alkhamis <69820410+Alkamis@users.noreply.github.com>
2020-09-13 12:29:54 +0300

    Update ax-hashing.py

--- ax-hashing.py
+++ ax-hashing.py
@@ -1,5 +1,6 @@
 import hashlib
 import pyfiglet
+
 from termcolor import cprint
 
 cprint(pyfiglet.figlet_format('HASHING', font='starwars'),
@@ -7,8 +8,20 @@ cprint(pyfiglet.figlet_format('HASHING', font='starwars'),
 
 mnue = 1
 
-while mnue < 4 :
-    mnue = int(input("Choose The Hash Type : \n 1- md5sum \n 2- sha1sum \n 3- sha256sum \n 4- Exit \n "))
+while True:
+
+    try:
+        mnue = int(input("Choose The Hash Type : \n 1- md5sum \n 2- sha1sum \n 3- sha256sum \n 4- Exit \n "))
+
+    except ValueError:
+        print("Oops! that was no valid input. please try again...\n")
+        break
+
+
+
+
+
+
     if mnue == 1:
         text = str(input("Enter The Text You Want To Hash It : "))
         # the user will enter value to hash it
@@ -17,7 +30,7 @@ while mnue < 4 :
         # then sending to md5()
         result = hashlib.md5(text.encode())
 
-        # printing hash value 
+        # printing hash value
         print("The md5sum Hash is : ", end='')
         print(result.hexdigest()) # end if 1
         print("--------------------------------md5sum-----------------------------------")
